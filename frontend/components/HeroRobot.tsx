0dcf01a66fb27333ce55b3a0a6a87691fb38a2f5
RosieOh <dhxogns920@gmail.com>
2025-12-28 14:29:55 +0900

    FEAT: 히어로 로봇 컴포넌트 구현 - 랜딩 페이지 히어로 섹션 UI

---
+++ frontend/components/HeroRobot.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+export default function HeroRobot() {
+  return (
+    <div className="absolute inset-0 z-0 w-full h-full overflow-hidden">
+      <iframe
+        src="https://my.spline.design/nexbotrobotcharacterconcept-Mu1XiRzONHbkJyjSPqGS79ut/"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+        className="pointer-events-auto"
+        style={{ 
+          opacity: 1,
+          filter: 'contrast(1.2) brightness(0.9) saturate(1.1)',
+        }}
+        allow="fullscreen"
+      ></iframe>
+    </div>
+  )
+}
